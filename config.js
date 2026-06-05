2b4c9dce6327a5deeecce17ae89d9beba5b1bcf5
Langerfrosch <78690793+Langerfrosch@users.noreply.github.com>
2026-06-05 13:54:43 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+// Repo-Konfiguration (kein Token hier – wird im Gerät gespeichert)
+var GH_OWNER = 'Langerfrosch';
+var GH_REPO  = 'KoordinatenVerwaltung';
+var GH_PAT   = localStorage.getItem('gh_pat') || '';
