752cdb17beef84bd6f4da89b1811fb0e03084ed6
Langerfrosch <78690793+Langerfrosch@users.noreply.github.com>
2026-06-05 13:53:32 +0200

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-// Automatisch generiert – nicht manuell bearbeiten
-var GH_OWNER = 'Langerfrosch';
-var GH_REPO  = 'KoordinatenVerwaltung';
-var GH_PAT   = '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';
